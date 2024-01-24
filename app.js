b3a1a9acf4792ec15e9c36b4669b9ccf033e2b8a
quntaoz <quntaozheng@gmail.com>
2024-01-23 22:26:35 -0500

    ran linter

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
+const express = require('express');
 
-const mongoUrl = ''
-mongoose.connect(mongoUrl)
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs');
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
+const mongoUrl = '';
+mongoose.connect(mongoUrl);
 
-module.exports = app
+app.use(cors());
+app.use(express.json());
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const app = require('./app')
-const logger = require('./utils/logger')
+const app = require('./app');
+const logger = require('./utils/logger');
 
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
-})
+  logger.info(`Server running on port ${PORT}`);
+});
