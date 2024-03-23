b1f1aa76586099aea1b2db0f9007912b8f4cce54
quntaoz <quntaozheng@gmail.com>
2024-03-23 19:14:33 -0400

    completed 4b adding test for api delete and update

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,19 +1,46 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs);
-    });
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body);
+  const { body } = request;
 
-  const savedBlog = await blog.save();
-  response.status(201).json(savedBlog);
+  if (body.title == null || body.url == null) {
+    response.status(400).json({ error: 'Title and URL are required' });
+  } else {
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0,
+    });
+
+    const savedBlog = await blog.save();
+    response.status(201).json(savedBlog);
+  }
+});
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { body } = request;
+
+  const updatedBlog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+  });
+
+  await Blog.findByIdAndDelete(request.params.id, updatedBlog, { new: true, runValidators: true, context: 'query' });
+  response.json(updatedBlog);
 });
 
 module.exports = blogsRouter;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,5 +1,5 @@
 const {
-  test, after, beforeEach, expect,
+  test, after, beforeEach, describe,
 } = require('node:test');
 const assert = require('node:assert');
 const mongoose = require('mongoose');
@@ -39,62 +39,111 @@ test('property id exist', async () => {
   response.body.forEach((blog) => assert.ok(blog.id !== undefined, 'blog.id is not defined'));
 });
 
-test('a valid blog can be added', async () => {
-  const newBlog = new Blog({
-    _id: '5a422a851b54a676234d17fd',
-    title: 'Awesome Blog Post',
-    author: 'Awesome Author',
-    url: 'https://awesomeblog.com/',
-    likes: 10,
-    __v: 0,
+describe('addition of new blog', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      _id: '5a422a851b54a676234d17fd',
+      title: 'Awesome Blog Post',
+      author: 'Awesome Author',
+      url: 'https://awesomeblog.com/',
+      likes: 10,
+      __v: 0,
+    };
+
+    await api.post('/api/blogs').send(newBlog).expect(201).expect('Content-Type', /application\/json/);
+
+    const response = await api.get('/api/blogs');
+
+    const titles = response.body.map((blog) => blog.title);
+
+    assert.strictEqual(response.body.length, blogList.length + 1);
+
+    assert(titles.includes('Awesome Blog Post'));
   });
 
-  await api.post('/api/blogs').send(newBlog.toJSON()).expect(201).expect('Content-Type', /application\/json/);
+  test('blog without likes can be added', async () => {
+    const newBlog = {
+      _id: '5a422a851b54a676234d17fd',
+      title: 'Awesome Blog Post',
+      author: 'Awesome Author',
+      url: 'https://awesomeblog.com/',
+    };
 
-  const response = await api.get('/api/blogs');
+    await api.post('/api/blogs').send(newBlog).expect(201).expect('Content-Type', /application\/json/);
 
-  const titles = response.body.map((blog) => blog.title);
+    const response = await api.get('/api/blogs');
 
-  assert.strictEqual(response.body.length, blogList.length + 1);
+    const titles = response.body.map((blog) => blog.title);
 
-  assert(titles.includes('Awesome Blog Post'));
-});
+    assert.strictEqual(response.body.length, blogList.length + 1);
 
-test('blog without likes can be added', async () => {
-  const newBlog = new Blog({
-    _id: '5a422a851b54a676234d17fd',
-    title: 'Awesome Blog Post',
-    author: 'Awesome Author',
-    url: 'https://awesomeblog.com/',
+    assert(titles.includes('Awesome Blog Post'));
   });
 
-  await api.post('/api/blogs').send(newBlog.toJSON()).expect(201).expect('Content-Type', /application\/json/);
+  test('blog without title are not added', async () => {
+    const newBlog = {
+      _id: '5a422a851b54a676234d17fd',
+      author: 'Awesome Author',
+      url: 'https://awesomeblog.com/',
+    };
 
-  const response = await api.get('/api/blogs');
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400);
 
-  const titles = response.body.map((blog) => blog.title);
+    const response = await api.get('/api/blogs');
 
-  assert.strictEqual(response.body.length, blogList.length + 1);
+    assert.strictEqual(response.body.length, blogList.length);
+  });
 
-  assert(titles.includes('Awesome Blog Post'));
+  test('blog without url are not added', async () => {
+    const newBlog = {
+      title: 'Awesome Blog Post',
+      _id: '5a422a851b54a676234d17fd',
+      author: 'Awesome Author',
+    };
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400);
+
+    const response = await api.get('/api/blogs');
+
+    assert.strictEqual(response.body.length, blogList.length);
+  });
 });
 
-test('blog without title or url are not added', async () => {
-  const newBlog = {
-    _id: '5a422a851b54a676234d17fd',
-    author: 'Awesome Author',
-    url: 'https://awesomeblog.com/',
-  }
+describe('deletion of new blog', () => {
+  test('deleting a single blog post', async () => {
+    const deleteBlogId = blogList[0]._id;
 
-  await api
-  .post('/api/blogs')
-  .send(newBlog)
-  .expect(400)
+    await api.delete(`/api/blogs/${deleteBlogId}`).expect(204);
 
-  const response = await api.get('/api/blog')
+    const response = await api.get('/api/blogs');
 
-  assert.strictEqual(response.body.length, blogList.length)
+    assert.strictEqual(response.body.length, blogList.length - 1);
+  });
+});
 
+describe('updating blog', () => {
+  test('updating likes of a blog', async () => {
+    const deleteBlogId = blogList[0]._id;
+
+    const updatedBlog = {
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 10,
+    };
+
+    const response = await api.put(`/api/blogs/${deleteBlogId}`).send(updatedBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    assert.strictEqual(response.body.likes, 10);
+  });
 });
 
 after(async () => {
